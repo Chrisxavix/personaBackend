64b9f7b0ec592439f19c2b85278fcf6a4d0e17b8
Chris <chrischillogallid64@outlook.com>
2021-06-02 15:47:04 -0500

    feat: cors configurations

--- personaBackend/src/main/java/com/example/personaBackend/PersonaBackendApplication.java
+++ personaBackend/src/main/java/com/example/personaBackend/PersonaBackendApplication.java
@@ -3,6 +3,8 @@ package com.example.personaBackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PersonaBackendApplication {
@@ -11,4 +13,16 @@ public class PersonaBackendApplication {
 		SpringApplication.run(PersonaBackendApplication.class, args);
 	}
 
+	/* Permisos de Cors */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				/* Funciona para todos los métodos al usar "/**" */
+				/* La ruta por el cual tiene permiso: http://localhost:4200 */
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
